9c5ae7ac2b093dc722b7adfa11d9b3f67b7e244f
st61014 <st61014@upce.cz>
2023-02-15 16:13:02 +0100

    finished RequestBody method

--- src/main/java/cz/upce/fei/NNPIA_CV01/objects/RequestObject.java
+++ src/main/java/cz/upce/fei/NNPIA_CV01/objects/RequestObject.java
@@ -1,14 +1,12 @@
 package cz.upce.fei.NNPIA_CV01.objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class RequestObject {
     String contents;
 }
