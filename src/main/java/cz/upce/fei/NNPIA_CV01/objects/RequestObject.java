ac7868439fd08789681a51efcb71a4df886a6631
st61014 <st61014@upce.cz>
2023-02-15 16:11:39 +0100

    Path, Querry, RequestBody methods

---
+++ src/main/java/cz/upce/fei/NNPIA_CV01/objects/RequestObject.java
@@ -0,0 +1,14 @@
+package cz.upce.fei.NNPIA_CV01.objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class RequestObject {
+    String contents;
+}
