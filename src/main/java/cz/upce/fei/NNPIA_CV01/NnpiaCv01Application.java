02542879f2549218d9cdb4811b86f9ede556c6ed
st61014 <st61014@upce.cz>
2023-02-15 15:10:53 +0100

    Innit commit

---
+++ src/main/java/cz/upce/fei/NNPIA_CV01/NnpiaCv01Application.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.NNPIA_CV01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaCv01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnpiaCv01Application.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/fei/NNPIA_CV01/NnpiaCv01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.NNPIA_CV01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCv01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
