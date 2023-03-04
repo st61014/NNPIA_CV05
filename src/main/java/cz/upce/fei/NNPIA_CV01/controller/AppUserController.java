9433d9e38857ff649d146bd488c257709ed345ef
st61014 <st61014@upce.cz>
2023-03-04 13:43:39 +0100

    steps 1. to 5.

--- src/main/java/cz/upce/fei/NNPIA_CV01/controller/AppUserController.java
+++ src/main/java/cz/upce/fei/NNPIA_CV01/controller/AppUserController.java
@@ -2,10 +2,11 @@ package cz.upce.fei.NNPIA_CV01.controller;
 
 import cz.upce.fei.NNPIA_CV01.domain.AppUser;
 import cz.upce.fei.NNPIA_CV01.repository.AppUserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -20,5 +21,31 @@ public class AppUserController {
     @GetMapping("")
     public List<AppUser> findAll(){
         return appUserRepository.findAllByActiveEquals(true);
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<AppUser> findUserByID(@RequestParam Long id){
+        return appUserRepository.findById(id).map(appUser -> ResponseEntity
+                        .ok().body(appUser))
+                        .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Object> createNewUser(@RequestBody AppUser appUser){
+        appUserRepository.save(appUser);
+        return ResponseEntity.ok().build();
+    }
+    @PutMapping("")
+    public ResponseEntity<Object> updateUser(@RequestBody AppUser appUser){
+        appUserRepository.updateAppUserById(appUser.getUserID(), appUser.getUsername(), appUser.getPassword(), appUser.getActive() , LocalDateTime.now());
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<Object> deleteUser(@RequestParam Long id){
+        appUserRepository.findById(id).ifPresent(appUser -> {
+            appUserRepository.delete(appUser);
+        });
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/cz/upce/fei/NNPIA_CV01/repository/AppUserRepository.java
+++ src/main/java/cz/upce/fei/NNPIA_CV01/repository/AppUserRepository.java
@@ -5,10 +5,12 @@ import cz.upce.fei.NNPIA_CV01.domain.Role;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface AppUserRepository extends PagingAndSortingRepository<AppUser, Long> {
     List<AppUser> findAllByActiveEquals(Boolean active);
     List<AppUser> findAllByRolesContaining(final Role role);
+    void updateAppUserById(Long id, String username, String password, Boolean active, LocalDateTime update_date);
 }
