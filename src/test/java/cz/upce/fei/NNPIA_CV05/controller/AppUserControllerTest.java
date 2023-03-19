b9f9d2180623846304a64abf94b3345c73813f1c
st61014 <st61014@upce.cz>
2023-03-19 17:06:22 +0100

    HTTP Request testing

---
+++ src/test/java/cz/upce/fei/NNPIA_CV05/controller/AppUserControllerTest.java
@@ -0,0 +1,50 @@
+package cz.upce.fei.NNPIA_CV05.controller;
+
+import cz.upce.fei.NNPIA_CV05.Example;
+import cz.upce.fei.NNPIA_CV05.domain.AppUser;
+import cz.upce.fei.NNPIA_CV05.repository.AppUserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:applications.yml")
+class AppUserControllerTest {
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+    @Autowired
+    private MockMvc mockMvc;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {appUserRepository.deleteAll();}
+
+    @Test
+    void findUserByIDCode200() throws Exception {
+        AppUser user = appUserRepository.save(Example.EXISTING_USER);
+        mockMvc.perform(MockMvcRequestBuilders.get("localhost:" + (int)((Math.random()*3000)+5000) + "/app-user/" + user.getUserID()))
+                .andDo(print()).
+                andExpect(status().isOk());
+    }
+    @Test
+    void findUserByIDCode404() throws Exception {
+        AppUser user = appUserRepository.save(Example.EXISTING_USER);
+        mockMvc.perform(MockMvcRequestBuilders.get("localhost:" + (int)((Math.random()*3000)+5000) + "/app-user/" + user.getUserID()))
+                .andDo(print()).
+                andExpect(status().isNotFound());
+    }
+}
